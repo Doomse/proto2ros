8124d40efebac3e58f2442a8b989f6049fa264c0
Doomse <dominik.hoefer@fkie.fraunhofer.de>
2026-08-05 11:51:08 +0000

    Remove deprecated ament_target_dependencies

--- proto2ros/cmake/proto2ros_vendor_package.cmake
+++ proto2ros/cmake/proto2ros_vendor_package.cmake
@@ -106,9 +106,14 @@ macro(proto2ros_vendor_package target)
     ${build_include_directories} "$<INSTALL_INTERFACE:include/${PROJECT_NAME}>"
   )
   rosidl_get_typesupport_target(cpp_interfaces ${target} "rosidl_typesupport_cpp")
-  target_link_libraries(${target}_conversions ${cpp_interfaces} ${ARG_CPP_DEPENDENCIES})
-  ament_target_dependencies(${target}_conversions
-    ${ARG_ROS_DEPENDENCIES} builtin_interfaces proto2ros rclcpp)
+  target_link_libraries(${target}_conversions 
+    ${cpp_interfaces} 
+    ${builtin_interfaces_TARGETS}
+    ${proto2ros_TARGETS}
+    proto2ros::proto2ros_conversions
+    rclcpp::rclcpp
+    ${ARG_CPP_DEPENDENCIES}
+  )
 
   find_program(CLANG_TIDY_EXECUTABLE NAMES "clang-tidy")
   if(BUILD_TESTING AND NOT ARG_NO_LINT AND CLANG_TIDY_EXECUTABLE)
